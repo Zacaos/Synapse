ab87c9cd0032469b6af994aede2def0e18490c07
Zacão <149967638+Zacaos@users.noreply.github.com>
2026-08-14 10:12:35 -0300

    synapse pix antifraud read

--- apps.py
+++ apps.py
@@ -215,7 +215,7 @@ if menu=='Synapse Dashboard':
     st.subheader('Transações Monitoradas')
     st.dataframe(DF.head(100),use_container_width=True);
 
-if menu=='Cadastro de empresa'
+elif menu=='Cadastro de empresa'
 
 #Cadastro da empresa#
 
